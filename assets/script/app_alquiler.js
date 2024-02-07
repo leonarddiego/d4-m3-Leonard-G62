ff26fda40d11d54a0c21b0fe152512f4f4db5ff5
Leonard <leonarddiegomc@gmail.com>
2024-02-06 23:17:21 -0300

    subiendo archivos

---
+++ assets/script/app_alquiler.js
@@ -0,0 +1,70 @@
+const container = document.querySelector('#alquiler .row');
+
+let cards = '';
+
+propiedadesAlquiler.forEach(prop => {
+  renderCards(prop)
+})
+
+container.innerHTML = cards
+
+function renderCards(prop){
+  let petHTML = checkPets(prop);
+  let smokeHTML = checkSmoke(prop);
+  
+  cards += `
+  <div class="col-md-4 mb-4">
+    <div class="card">
+      <img
+        src="./assets/img/${prop.src}.jpg"
+        class="card-img-top"
+        alt="${prop.nombre}"
+      />
+      <div class="card-body">
+        <h5 class="card-title">${prop.nombre}</h5>
+        <p class="card-text">${prop.descripcion}</p>
+        <p><i class="fas fa-map-marker-alt"></i> ${prop.ubicacion}</p>
+        <p>
+          <i class="fas fa-bed"></i> ${prop.habitaciones} Habitaciones |
+          <i class="fas fa-bath"></i> ${prop.banos} Baños
+        </p>
+        <p><i class="fas fa-dollar-sign"></i> ${prop.costo}</p>
+        ${smokeHTML}
+        ${petHTML}
+      </div>
+    </div>
+  </div>
+  `
+}
+
+function checkPets(propiedad){
+  if (propiedad.pets){
+    return `
+    <p class="text-success">
+    <i class="fas fa-paw"></i> Mascotas permitidas
+    </p>
+    `
+  } else {
+    return `
+    <p class="text-danger">
+    <i class="fa-solid fa-ban"></i> No se permiten mascotas
+    </p>
+    `
+  }
+}
+
+function checkSmoke(propiedad){
+  if (propiedad.smoke){
+    return `
+    <p class="text-success">
+    <i class="fas fa-smoking"></i> Permitido fumar
+    </p>
+    `
+  } else {
+    return `
+    <p class="text-danger">
+    <i class="fas fa-smoking-ban"></i> No se permite fumar
+    </p>
+    `
+  }
+}
